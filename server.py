eec0143d17abda53c018b0bef954e62c9a3f7b1e
carlos <ccarbonell@gmail.com>
2024-01-24 16:44:15 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! roomba server'
+    return 'TONTO EL QUE LO LEA'
 
 # main driver function
 if __name__ == '__main__':
  
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
