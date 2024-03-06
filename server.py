3c3cc1980a1886391a13206856c6e78ef05bac38
carlos <ccarbonell@gmail.com>
2024-03-06 22:23:53 +0100

    Update server.py

--- server.py
+++ server.py
@@ -7,16 +7,12 @@ from flask_cors import CORS
 import threading
 from multiprocessing import Process, Queue
 
-
-from irobot_edu_sdk.robots import event, hand_over, Color, Robot, Root, Create3
-from irobot_edu_sdk.backend.bluetooth import Bluetooth
-
 app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app,cors_allowed_origins="*")
 
-
+q = Queue(maxsize=10)  # Create a queue
 
 @app.route('/')
 def index():
@@ -33,13 +29,14 @@ def handle_message(message):
     print("got message",message)
     #roomba.docommand(message)
     q.put(message)
+    print(q)
     emit('message', message, broadcast=True, include_self=False)
     
 
 # WebSocket events for signaling would go here
 
 if __name__ == '__main__':
-    q = Queue(maxsize=10)  # Create a queue
+    
     p = Process(target=startroomba, args=(q,))  # Set up the child process with the queue
     p.start()  # Start the child process
     socketio.run(app, debug=True,host='0.0.0.0', port=5000)
