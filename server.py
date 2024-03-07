f7a7774200b0df2b8bfbfe3440e669aebd000bc7
carlos <ccarbonell@gmail.com>
2024-03-07 09:11:10 +0100

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,8 @@ CORS(app)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app,cors_allowed_origins="*")
 
-q = Queue(maxsize=10)  # Create a queue
+q = Queue(maxsize=1)  # Create a queue
+direction=""
 
 @app.route('/')
 def index():
@@ -25,18 +26,26 @@ def connected(message):
 @socketio.on('message')
 def handle_message(message):
     global q
+    global direction
     # Just emit the received message to all connected clients except the sender
     print("got message",message)
     #roomba.docommand(message)
-    q.put(message)
-    print(q)
+    direction=message
+    #q.put(message)
+    #print(q)
     emit('message', message, broadcast=True, include_self=False)
     
 
 # WebSocket events for signaling would go here
 
+def setdirectionLoop():
+    while True:
+        q.put(direction)
+
+
 if __name__ == '__main__':
-    
+    x = threading.Thread(target=setdirectionLoop)
+    x.start()
     p = Process(target=startroomba, args=(q,))  # Set up the child process with the queue
     p.start()  # Start the child process
     socketio.run(app, debug=True,host='0.0.0.0', port=5000)
